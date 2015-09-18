6405ef062f86ed430398cdf9540910ae47de1d89
Salyards, Adey <adey.salyards@capitalone.com>
2015-09-18 07:07:58 -0400

    Initial Commit

---
+++ Tips_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips_v2
+//
+//  Created by Salyards, Adey on 9/18/15.
+//  Copyright (c) 2015 Salyards, Adey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
